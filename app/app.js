d502a625dd76e1f918154cbacace9a74aabdcd0a
Shams Ali <shamstali1@gmail.com>
2025-01-27 14:15:17 +0000

    Implement getTopics API

--- app/app.js
+++ app/app.js
@@ -1,15 +1,29 @@
 const express = require("express");
-const { getEndpoints } = require("../controllers/api.controller");
+const { getEndpoints, getTopics } = require("../controllers/api.controller");
 const app = express();
 
 // Reminder to add express.us to access response body
 
 // Get Requests
 app.get("/api", getEndpoints);
+app.get("/api/topics", getTopics);
 
 // Error Handling
+// Error 400
+// app.use((err, request, response, next) => {
+//   response
+//     .status(400)
+//     .send({msg: "Bad Request"})
+//     .catch((err) => {
+//       next(err);
+//     });
+// });
+
+// Default 500 response
 app.use((err, request, response, next) => {
-  response.status(500).send(err);
+  response
+    .status(500)
+    .send({ msg: "Sorry bud, internal server error, have a good day" });
 });
 
 module.exports = app;
