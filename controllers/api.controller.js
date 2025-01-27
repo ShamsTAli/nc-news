7b6b8aa451ec285f218ed6825da18a442c703a4a
Shams Ali <shamstali1@gmail.com>
2025-01-27 12:16:24 +0000

    Implemet a working API endpoint to retrieve Endpoints

---
+++ controllers/api.controller.js
@@ -0,0 +1,11 @@
+const { fetchEndpoints } = require("../models/api.model");
+
+exports.getEndpoints = (request, response, next) => {
+  fetchEndpoints()
+    .then((endpoints) => {
+      response.status(200).send({ endpoints });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
--- db/seeds/run-seed.js
+++ db/seeds/run-seed.js
@@ -1,9 +1,11 @@
 const devData = require('../data/development-data/index.js');
+const testData = require("../data/test-data/index.js")
 const seed = require('./seed.js');
 const db = require('../connection.js');
+const data = process.env.NODE_ENV === "test" ? testData : devData
 
 const runSeed = () => {
-  return seed(devData).then(() => db.end());
+  return seed(data).then(() => db.end());
 };
 
 runSeed();
