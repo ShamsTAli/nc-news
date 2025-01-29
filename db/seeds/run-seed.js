d620c2b844a99c62368e642926d61755cd3d9683
Shams Ali <shamstali1@gmail.com>
2025-01-29 13:42:39 +0000

    Deleted project, and obtained a new database url as was having password issues

--- db/seeds/run-seed.js
+++ db/seeds/run-seed.js
@@ -1,9 +1,9 @@
-const devData = require('../data/development-data/index.js');
-const testData = require("../data/test-data/index.js")
-const seed = require('./seed.js');
-const db = require('../connection.js');
-const data = process.env.NODE_ENV === "test" ? testData : devData
-
+const devData = require("../data/development-data/index.js");
+const testData = require("../data/test-data/index.js");
+const seed = require("./seed.js");
+const db = require("../connection.js");
+const data = process.env.NODE_ENV === "test" ? testData : devData;
+console.log(process.env.NODE_ENV);
 const runSeed = () => {
   return seed(data).then(() => db.end());
 };
